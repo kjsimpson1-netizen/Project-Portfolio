9122e5ef78815c2f0d9492cbf37f78c01060ec61
Kyle Simpson <kjsimpson1@outlook.com>
2026-09-10 12:55:15 -0700

    More general updates

--- js/script.js
+++ js/script.js
@@ -117,3 +117,9 @@ document.addEventListener("click", function (event) {
   });
 
 });
+
+fetch("components/navbar.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("navbar").innerHTML = data;
+  });
